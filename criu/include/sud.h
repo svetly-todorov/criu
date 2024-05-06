34dca84592445d876f2a7fbb29fb9a517cab17e5
Svetly Todorov <svetly.todorov@memverge.com>
2024-05-06 20:35:31 +0000

    sud.c: add scaffolding for restore

--- criu/include/sud.h
+++ criu/include/sud.h
@@ -29,6 +29,8 @@ struct sys_dispatch_entry {
 extern struct sys_dispatch_entry *sud_lookup(pid_t tid_real, bool create, bool mandatory);
 #define sud_find_entry(tid_real) sud_lookup(tid_real, false, true)
 extern int sud_collect_entry(pid_t tid_real);
+extern int sud_read_image(void);
+extern int restore_sud_per_core(pid_t tid_real);
 extern int dump_sud_per_core(pid_t tid_real, ThreadCoreEntry *tc);
 extern int dump_sud(void);
 
